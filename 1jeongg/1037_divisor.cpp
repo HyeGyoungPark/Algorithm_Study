fbaefa0dd9f035416426a0cf80f094237e0c7b9c
Nam WoeunJung <84652886+1jeongg@users.noreply.github.com>
2022-01-29 00:19:54 +0900

    [BOJ] 약수 / 실버5 / 15분
    https://www.acmicpc.net/problem/1037

---
+++ 1jeongg/1037_divisor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n,arr[50];
+	int min_num=1000000, max_num=0;
+	
+	cin>>n;
+	
+	for (int i=0; i<n; i++){
+		cin>>arr[i];
+		if (arr[i] < min_num) min_num = arr[i];
+		if (arr[i] > max_num) max_num = arr[i];
+	}
+	
+	int answer = min_num*max_num;
+	cout<<answer;
+	
+	return 0;
+}
